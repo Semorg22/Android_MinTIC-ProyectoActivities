cc8b2140ce3fffc7c28859192b3b60e3537da782
Semorg22 <sebasmorenog.22@gmail.com>
2021-11-01 22:30:43 -0500

    Initial commit

---
+++ app/src/main/java/com/smdeveloper/proyectoactivities/MainActivity.java
@@ -0,0 +1,69 @@
+package com.smdeveloper.proyectoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show(); //Crear un mini mensaje
+        Log.i("Información","onCreate"); //console log normal, para ver cositas en la consola
+         */
+    }
+
+    public void goToActivitySecond(View view) { //movernos entre activities
+        Intent newIntent = new Intent(this,SecondActivity.class);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //evita crear pestañas continuas
+        newIntent.putExtra("msg","Hola");
+        newIntent.putExtra("year",2021);
+        startActivity(newIntent);
+    }
+
+    /*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Información","onDestroy");
+    }
+
+     */
+}
