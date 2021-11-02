d3b4459794a31e510244b28316d2362ff407e158
Semorg22 <sebasmorenog.22@gmail.com>
2021-11-02 09:19:40 -0500

    Creación dialogo para confirmar salida de la app

--- app/src/main/java/com/smdeveloper/ProyectoAndroid_MinTIC/MainActivity.java
+++ app/src/main/java/com/smdeveloper/ProyectoAndroid_MinTIC/MainActivity.java
@@ -1,9 +1,14 @@
 package com.smdeveloper.ProyectoAndroid_MinTIC;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +29,47 @@ public class MainActivity extends AppCompatActivity {
         newIntent.putExtra("msg","Hola MinTIC");
         newIntent.putExtra("year",2021);
         startActivity(newIntent);
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) { //Confirmación de salir de la app, dialogo como optionPane
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            new AlertDialog.Builder(this,R.style.Theme_AppCompat_DayNight_Dialog_Alert)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("Información")
+                    .setMessage("¿Desea salir?")
+                    .setNegativeButton("No",null)
+                    .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            MainActivity.this.finish();
+                        }
+                    }).show();
+            return true;
+        }
+        return super.onKeyDown(keyCode,event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { //poner el menu que creamos en el actionBar
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        if(id == R.id.mnu_second_activity) {
+            Intent newIntent = new Intent(this,SecondActivity.class);
+            newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //evita crear pestañas continuas
+            newIntent.putExtra("msg","Hola MinTIC");
+            newIntent.putExtra("year",2021);
+            startActivity(newIntent);
+        }
+        else if(id == R.id.mnu_third_activity) {
+            Intent newIntent = new Intent(this,ThirdActivity.class);
+            newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //evita crear pestañas continuas
+            startActivity(newIntent);
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 
     /*
--- app/src/main/java/com/smdeveloper/ProyectoAndroid_MinTIC/ThirdActivity.java
+++ app/src/main/java/com/smdeveloper/ProyectoAndroid_MinTIC/ThirdActivity.java
@@ -3,6 +3,7 @@ package com.smdeveloper.ProyectoAndroid_MinTIC;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Soporte de actionBar.mostrar
+    }
+
+    //Destruir la app
+    public void onBackPressed() {
+        finish();
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        if(id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 }
